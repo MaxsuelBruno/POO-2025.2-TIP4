2fc2bf54056f3400585e44d21e9f69f4d3676aeb
Maxsuel Bruno <maxsuel.bruno10@aluno.ifce.edu.br>
2025-09-30 12:05:22 -0300

    Add files via upload

---
+++ Lista-02/Capitulo-04/Exercicios-Propostos/Q04/src/br/edu/principal/Principal.java
@@ -0,0 +1,34 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Digite o primeiro número: ");
+	        int numero1 = scanner.nextInt();
+
+	        System.out.print("Digite o segundo número: ");
+	        int numero2 = scanner.nextInt();
+
+	        System.out.print("Digite o terceiro número: ");
+	        int numero3 = scanner.nextInt();
+
+	        int maior = numero1;
+
+	        if (numero2 > maior) {
+	            maior = numero2;
+	        }
+
+	        if (numero3 > maior) {
+	            maior = numero3;
+	        }
+
+	        System.out.println("O maior número é: " + maior);
+
+	}
+
+}
